83afaa2857b2e440a5008849668e6df3f063beae
zeroproject1 <88389125+zeroproject1@users.noreply.github.com>
2021-08-05 23:37:40 +0700

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -10,28 +10,380 @@ fi
 red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
-MYIP=$(wget -qO- ifconfig.me/ip);
-IZIN=$( curl http://akses.zero-vpn-stores.tech:81/aksesvpnstore | grep $MYIP )
-if [ $MYIP = $IZIN ]; then
-echo -e "${green}Permission Accepted...${NC}"
-else
-echo -e "${red}Permission Denied!${NC}";
-echo "Please Contact Admin"
-echo "Telegram t.me/bokirtampan"
-rm -f setup.sh
-exit 0
-fi
+MYIP=$(wget -qO- ifconfig.me/ip); 
 if [ -f "/etc/v2ray/domain" ]; then
 echo "Script Already Installed"
 exit 0
 fi
+clear
+mkdir /var/lib/premium-script;
+echo "IP=" >> /var/lib/premium-script/ipvps.conf
+
+# MengInstall SSH
+export DEBIAN_FRONTEND=noninteractive
+MYIP=$(wget -qO- ipv4.wildyproject.com);
+MYIP2="s/xxxxxxxxx/$MYIP/g";
+NET=$(ip -o $ANU -4 route show to default | awk '{print $5}');
+source /etc/os-release
+ver=$VERSION_ID
+
+#detail nama perusahaan
+country=ID
+state=Indonesia
+locality=Indonesia
+organization=Selalu
+organizationalunit=Baca
+commonname=Bismillah
+email=paoandest@gmail.com
+
+# simple password minimal
+wget -O /etc/pam.d/common-password "https://raw.githubusercontent.com/zeroproject1/vpn/main/password"
+chmod +x /etc/pam.d/common-password
+
+# go to root
+cd
+
+# Edit file /etc/systemd/system/rc-local.service
+cat > /etc/systemd/system/rc-local.service <<-END
+[Unit]
+Description=/etc/rc.local
+ConditionPathExists=/etc/rc.local
+[Service]
+Type=forking
+ExecStart=/etc/rc.local start
+TimeoutSec=0
+StandardOutput=tty
+RemainAfterExit=yes
+SysVStartPriority=99
+[Install]
+WantedBy=multi-user.target
+END
+
+# nano /etc/rc.local
+cat > /etc/rc.local <<-END
+#!/bin/sh -e
+# rc.local
+# By default this script does nothing.
+exit 0
+END
+
+# Ubah izin akses
+chmod +x /etc/rc.local
+
+# enable rc local
+systemctl enable rc-local
+systemctl start rc-local.service
+
+# disable ipv6
+echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
+sed -i '$ i\echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6' /etc/rc.local
+
+#update
+apt update -y
+apt upgrade -y
+apt dist-upgrade -y
+apt-get remove --purge ufw firewalld -y
+apt-get remove --purge exim4 -y
+
+# install wget and curl
+apt -y install wget curl
+
+# set time GMT +7
+ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
+
+# set locale
+sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
+
+# install
+apt-get --reinstall --fix-missing install -y bzip2 gzip coreutils wget screen rsyslog iftop htop net-tools zip unzip wget net-tools curl nano sed screen gnupg gnupg1 bc apt-transport-https build-essential dirmngr libxml-parser-perl neofetch git lsof
+echo "clear" >> .profile
+echo "neofetch" >> .profile
+
+# install webserver
+apt -y install nginx
+cd
+rm /etc/nginx/sites-enabled/default
+rm /etc/nginx/sites-available/default
+wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/zeroproject1/vpn/main/nginx.conf"
+mkdir -p /home/vps/public_html
+wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/zeroproject1/vpn/main/vps.conf"
+/etc/init.d/nginx restart
+
+# install badvpn
+cd
+wget -O /usr/bin/badvpn-udpgw "https://raw.githubusercontent.com/zeroproject1/vpn/main/badvpn-udpgw64"
+chmod +x /usr/bin/badvpn-udpgw
+sed -i '$ i\screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7100 --max-clients 500' /etc/rc.local
+sed -i '$ i\screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200 --max-clients 500' /etc/rc.local
+sed -i '$ i\screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 500' /etc/rc.local
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7100 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7400 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7500 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7600 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7700 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7800 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7900 --max-clients 500
+
+# setting port ssh
+sed -i 's/Port 22/Port 22/g' /etc/ssh/sshd_config
+
+# install dropbear
+apt -y install dropbear
+sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=143/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 109"/g' /etc/default/dropbear
+echo "/bin/false" >> /etc/shells
+echo "/usr/sbin/nologin" >> /etc/shells
+/etc/init.d/dropbear restart
+
+# install squid
+cd
+apt -y install squid3
+wget -O /etc/squid/squid.conf "https://raw.githubusercontent.com/zeroproject1/vpn/main/squid3.conf"
+sed -i $MYIP2 /etc/squid/squid.conf
+
+# setting vnstat
+apt -y install vnstat
+/etc/init.d/vnstat restart
+apt -y install libsqlite3-dev
+wget https://humdi.net/vnstat/vnstat-2.6.tar.gz
+tar zxvf vnstat-2.6.tar.gz
+cd vnstat-2.6
+./configure --prefix=/usr --sysconfdir=/etc && make && make install
+cd
+vnstat -u -i $NET
+sed -i 's/Interface "'""eth0""'"/Interface "'""$NET""'"/g' /etc/vnstat.conf
+chown vnstat:vnstat /var/lib/vnstat -R
+systemctl enable vnstat
+/etc/init.d/vnstat restart
+rm -f /root/vnstat-2.6.tar.gz
+rm -rf /root/vnstat-2.6
+
+# install stunnel
+apt install stunnel4 -y
+cat > /etc/stunnel/stunnel.conf <<-END
+cert = /etc/stunnel/stunnel.pem
+client = no
+socket = a:SO_REUSEADDR=1
+socket = l:TCP_NODELAY=1
+socket = r:TCP_NODELAY=1
+
+[dropbear]
+accept = 222
+connect = 127.0.0.1:22
+
+[dropbear]
+accept = 777
+connect = 127.0.0.1:22
+
+[openvpn]
+accept = 442
+connect = 127.0.0.1:1194
+
+[wsssl]
+accept = 445
+connect = 700
+
+[trojango]
+accept = 443
+connect = 127.0.0.1:81
+
+END
+
+# make a certificate
+openssl genrsa -out key.pem 2048
+openssl req -new -x509 -key key.pem -out cert.pem -days 1095 \
+-subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$commonname/emailAddress=$email"
+cat key.pem cert.pem >> /etc/stunnel/stunnel.pem
+
+# konfigurasi stunnel
+sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
+/etc/init.d/stunnel4 restart
+
+#OpenVPN
+wget https://raw.githubusercontent.com/zeroproject1/vpn/main/vpn.sh && chmod +x vpn.sh && ./vpn.sh
+
+# install fail2ban
+apt -y install fail2ban
+
+# Instal DDOS Flate
+if [ -d '/usr/local/ddos' ]; then
+	echo; echo; echo "Please un-install the previous version first"
+	exit 0
+else
+	mkdir /usr/local/ddos
+fi
+clear
+echo; echo 'Installing DOS-Deflate 0.6'; echo
+echo; echo -n 'Downloading source files...'
+wget -q -O /usr/local/ddos/ddos.conf http://www.inetbase.com/scripts/ddos/ddos.conf
+echo -n '.'
+wget -q -O /usr/local/ddos/LICENSE http://www.inetbase.com/scripts/ddos/LICENSE
+echo -n '.'
+wget -q -O /usr/local/ddos/ignore.ip.list http://www.inetbase.com/scripts/ddos/ignore.ip.list
+echo -n '.'
+wget -q -O /usr/local/ddos/ddos.sh http://www.inetbase.com/scripts/ddos/ddos.sh
+chmod 0755 /usr/local/ddos/ddos.sh
+cp -s /usr/local/ddos/ddos.sh /usr/local/sbin/ddos
+echo '...done'
+echo; echo -n 'Creating cron to run script every minute.....(Default setting)'
+/usr/local/ddos/ddos.sh --cron > /dev/null 2>&1
+echo '.....done'
+echo; echo 'Installation has completed.'
+echo 'Config file is at /usr/local/ddos/ddos.conf'
+echo 'Please send in your comments and/or suggestions to zaf@vsnl.com'
+
+# banner /etc/issue.net
+echo "Banner /etc/issue.net" >>/etc/ssh/sshd_config
+sed -i 's@DROPBEAR_BANNER=""@DROPBEAR_BANNER="/etc/issue.net"@g' /etc/default/dropbear
+
+# blockir torrent
+iptables -A FORWARD -m string --string "get_peers" --algo bm -j DROP
+iptables -A FORWARD -m string --string "announce_peer" --algo bm -j DROP
+iptables -A FORWARD -m string --string "find_node" --algo bm -j DROP
+iptables -A FORWARD -m string --algo bm --string "BitTorrent" -j DROP
+iptables -A FORWARD -m string --algo bm --string "BitTorrent protocol" -j DROP
+iptables -A FORWARD -m string --algo bm --string "peer_id=" -j DROP
+iptables -A FORWARD -m string --algo bm --string ".torrent" -j DROP
+iptables -A FORWARD -m string --algo bm --string "announce.php?passkey=" -j DROP
+iptables -A FORWARD -m string --algo bm --string "torrent" -j DROP
+iptables -A FORWARD -m string --algo bm --string "announce" -j DROP
+iptables -A FORWARD -m string --algo bm --string "info_hash" -j DROP
+iptables-save > /etc/iptables.up.rules
+iptables-restore -t < /etc/iptables.up.rules
+netfilter-persistent save
+netfilter-persistent reload
+
+# download script
+cd /usr/bin
+wget -O add-host "https://raw.githubusercontent.com/zeroproject1/vpn/main/add-host.sh"
+wget -O about "https://raw.githubusercontent.com/zeroproject1/vpn/main/about.sh"
+wget -O menu "https://raw.githubusercontent.com/zeroproject1/vpn/main/menu.sh"
+wget -O usernew "https://raw.githubusercontent.com/zeroproject1/vpn/main/usernew.sh"
+wget -O trial "https://raw.githubusercontent.com/zeroproject1/vpn/main/trial.sh"
+wget -O hapus "https://raw.githubusercontent.com/zeroproject1/vpn/main/hapus.sh"
+wget -O member "https://raw.githubusercontent.com/zeroproject1/vpn/main/member.sh"
+wget -O delete "https://raw.githubusercontent.com/zeroproject1/vpn/main/delete.sh"
+wget -O cek "https://raw.githubusercontent.com/zeroproject1/vpn/main/cek.sh"
+wget -O restart "https://raw.githubusercontent.com/zeroproject1/vpn/main/restart.sh"
+wget -O speedtest "https://raw.githubusercontent.com/zeroproject1/vpn/main/speedtest_cli.py"
+wget -O info "https://raw.githubusercontent.com/zeroproject1/vpn/main/info.sh"
+wget -O ram "https://raw.githubusercontent.com/zeroproject1/vpn/main/ram.sh"
+wget -O renew "https://raw.githubusercontent.com/haluboys/xzvnct/main/renew.sh"
+wget -O autokill "https://raw.githubusercontent.com/zeroproject1/vpn/main/autokill.sh"
+wget -O ceklim "https://raw.githubusercontent.com/zeroproject1/vpn/main/ceklim.sh"
+wget -O tendang "https://raw.githubusercontent.com/zeroproject1/vpn/main/tendang.sh"
+wget -O clear-log "https://raw.githubusercontent.com/zeroproject1/vpn/main/clear-log.sh"
+wget -O change-port "https://raw.githubusercontent.com/zeroproject1/vpn/main/change.sh"
+wget -O port-ovpn "https://raw.githubusercontent.com/zeroproject1/vpn/main/port-ovpn.sh"
+wget -O port-ssl "https://raw.githubusercontent.com/zeroproject1/vpn/main/port-ssl.sh"
+wget -O port-wg "https://raw.githubusercontent.com/zeroproject1/vpn/main/port-wg.sh"
+wget -O port-tr "https://raw.githubusercontent.com/zeroproject1/vpn/main/port-tr.sh"
+wget -O port-sstp "https://raw.githubusercontent.com/zeroproject1/vpn/main/port-sstp.sh"
+wget -O port-squid "https://raw.githubusercontent.com/zeroproject1/vpn/main/port-squid.sh"
+wget -O port-ws "https://raw.githubusercontent.com/zeroproject1/vpn/main/port-ws.sh"
+wget -O port-vless "https://raw.githubusercontent.com/zeroproject1/vpn/main/port-vless.sh"
+wget -O wbmn "https://raw.githubusercontent.com/zeroproject1/vpn/main/webmin.sh"
+wget -O xp "https://raw.githubusercontent.com/zeroproject1/vpn/main/xp.sh"
+wget -O kernel-updt "https://raw.githubusercontent.com/zeroproject1/vpn/main/kernel-update.sh"
+wget -O bbr "https://raw.githubusercontent.com/zeroproject1/vpn/main/bbr.sh"
+wget -O /usr/bin/user-limit https://raw.githubusercontent.com/zeroproject1/vpn/main/user-limit.sh && chmod +x /usr/bin/user-limit
+wget -O cfd "https://raw.githubusercontent.com/zeroproject1/vpn/main/cfd.sh"
+wget -O cff "https://raw.githubusercontent.com/zeroproject1/vpn/main/cff.sh"
+wget -O cfh "https://raw.githubusercontent.com/zeroproject1/vpn/main/cfh.sh"
+wget -O certv2ray "https://raw.githubusercontent.com/zeroproject1/vpn/main/cert.sh"
+wget -O autoreboot "https://raw.githubusercontent.com/zeroproject1/vpn/main/autoreboot.sh"
+wget -O bannerku "https://raw.githubusercontent.com/zeroproject1/vpn/main/bannerku"
+
+chmod +x add-host
+chmod +x menu
+chmod +x usernew
+chmod +x trial
+chmod +x hapus
+chmod +x member
+chmod +x delete
+chmod +x cek
+chmod +x restart
+chmod +x speedtest
+chmod +x info
+chmod +x about
+chmod +x autokill
+chmod +x tendang
+chmod +x ceklim
+chmod +x ram
+chmod +x renew
+chmod +x clear-log
+chmod +x change-port
+chmod +x port-ovpn
+chmod +x port-ssl
+chmod +x port-wg
+chmod +x port-sstp
+chmod +x port-tr
+chmod +x port-squid
+chmod +x port-ws
+chmod +x port-vless
+chmod +x wbmn
+chmod +x xp
+chmod +x kernel-updt
+chmod +x bbr
+chmod +x cfd
+chmod +x cff
+chmod +x cfh
+chmod +x certv2ray
+chmod +x autoreboot
+chmod +x bannerku
+echo "0 5 * * * root clear-log && reboot" >> /etc/crontab
+echo "0 0 * * * root xp" >> /etc/crontab
+# remove unnecessary files
+cd
+apt autoclean -y
+apt -y remove --purge unscd
+apt-get -y --purge remove samba*;
+apt-get -y --purge remove apache2*;
+apt-get -y --purge remove bind9*;
+apt-get -y remove sendmail*
+apt autoremove -y
+# finishing
+cd
+chown -R www-data:www-data /home/vps/public_html
+/etc/init.d/nginx restart
+/etc/init.d/openvpn restart
+/etc/init.d/cron restart
+/etc/init.d/ssh restart
+/etc/init.d/dropbear restart
+/etc/init.d/fail2ban restart
+/etc/init.d/stunnel4 restart
+/etc/init.d/vnstat restart
+/etc/init.d/squid restart
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7100 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7400 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7500 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7600 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7700 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7800 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7900 --max-clients 500
+history -c
+echo "unset HISTFILE" >> /etc/profile
+
+cd
+rm -f /root/key.pem
+rm -f /root/cert.pem
+rm -f /root/ssh-vpn.sh
+
+# finihsing
+clear
+
+
 mkdir /var/lib/premium-script;
 echo "IP=" >> /var/lib/premium-script/ipvps.conf
 wget https://raw.githubusercontent.com/zeroproject1/vpn/main/cf.sh && chmod +x cf.sh && ./cf.sh
 #install ssh ovpn
 wget https://raw.githubusercontent.com/zeroproject1/vpn/main/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn ./ssh-vpn.sh
 wget https://raw.githubusercontent.com/zeroproject1/vpn/main/sstp.sh && chmod +x sstp.sh && screen -S sstp ./sstp.sh
-wget https://raw.githubusercontent.com/bokir-tampan/ranjau-darate/main/weleh.sh && chmod +x weleh.sh && screen -S weleh ./weleh.sh
 #install ssr
 wget https://raw.githubusercontent.com/zeroproject1/vpn/main/ssr.sh && chmod +x ssr.sh && screen -S ssr ./ssr.sh
 wget https://raw.githubusercontent.com/zeroproject1/vpn/main/sodosok.sh && chmod +x sodosok.sh && screen -S ss ./sodosok.sh
@@ -42,8 +394,9 @@ wget https://raw.githubusercontent.com/zeroproject1/vpn/main/ins-vt.sh && chmod 
 #install L2TP
 wget https://raw.githubusercontent.com/zeroproject1/vpn/main/ipsec.sh && chmod +x ipsec.sh && screen -S ipsec ./ipsec.sh
 wget https://raw.githubusercontent.com/zeroproject1/vpn/main/set-br.sh && chmod +x set-br.sh && ./set-br.sh
-#wget https://raw.githubusercontent.com/zeroproject1/vpn/main/ins-trojango.sh && chmod +x ins-trojango.sh && screen -S Trojan-go ./ins-trojango.sh
+wget https://raw.githubusercontent.com/zeroproject1/vpn/main/ins-trojango.sh && chmod +x ins-trojango.sh && screen -S Trojan-go ./ins-trojango.sh
 
+rm -f /root/cf.sh
 rm -f /root/ssh-vpn.sh
 rm -f /root/sstp.sh
 rm -f /root/wg.sh
@@ -56,7 +409,7 @@ rm -f /root/ins-trojango.sh
 cat <<EOF> /etc/systemd/system/autosett.service
 [Unit]
 Description=autosetting
-Documentation=https://red-flat.my.id
+Documentation=https://zero-vpn-stores.tech
 
 [Service]
 Type=oneshot
@@ -76,7 +429,7 @@ clear
 echo " "
 echo "Installation has been completed!!"
 echo " "
-echo "=================================-Autoscript Premium-===========================" | tee -a log-install.txt
+echo "=================================-HALUBOYS-===========================" | tee -a log-install.txt
 echo "" | tee -a log-install.txt
 echo "--------------------------------------------------------------------------------" | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
@@ -100,7 +453,7 @@ echo "   - V2RAY Vmess None TLS    : 80"  | tee -a log-install.txt
 echo "   - V2RAY Vless TLS         : 2083"  | tee -a log-install.txt
 echo "   - V2RAY Vless None TLS    : 8880"  | tee -a log-install.txt
 echo "   - Trojan                  : 2087"  | tee -a log-install.txt
-echo "   - TrojanGO                : 2096"  | tee -a log-install.txt
+echo "   - Trojango                : 443"  | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
 echo "   >>> Server Information & Other Features"  | tee -a log-install.txt
 echo "   - Timezone                : Asia/Jakarta (GMT +7)"  | tee -a log-install.txt
@@ -117,12 +470,12 @@ echo "   - Full Orders For Various Services" | tee -a log-install.txt
 echo "   - White Label" | tee -a log-install.txt
 echo "   - Installation Log --> /root/log-install.txt"  | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
-echo "   - Dev/Main                : Ceu Edoh Projectr"  | tee -a log-install.txt
-echo "   - Telegram                : T.me/bokirtampan"  | tee -a log-install.txt
-echo "   - Instagram               : @ands26"  | tee -a log-install.txt
-echo "   - Whatsapp                : 0895396172479"  | tee -a log-install.txt
-echo "   - Facebook                : https://www.facebook.com/ardiands" | tee -a log-install.txt
-echo "------------------Script Created By CEU EDOH PROJECT-----------------" | tee -a log-install.txt
+echo "   - Dev/Main                : HALUBOYS"  | tee -a log-install.txt
+echo "   - Telegram                : T.me/XZVNCT"  | tee -a log-install.txt
+echo "   - Instagram               : @XZVNCT"  | tee -a log-install.txt
+echo "   - Whatsapp                : 0838386165686"  | tee -a log-install.txt
+echo "   - Facebook                : https://www.facebook.com/" | tee -a log-install.txt
+echo "------------------Script Created By HALUBOYS-----------------" | tee -a log-install.txt
 echo ""
 echo " Reboot 15 Sec"
 sleep 15
